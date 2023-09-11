9ac65e308938edc71d8e66d44c5aa1757b1d449f
vikashkumar <vikashpoddar437@gmail.com>
2023-09-11 23:33:10 +0530

    js question

--- 8kyu/first.js
+++ 8kyu/first.js
@@ -1,1 +1,13 @@
-console.log("vikash");
+function countSheeps(sheep) {
+    // TODO
+     var count=0;
+    for(const value of sheep){
+     
+      if(value==true){
+   
+        count++;
+      }
+    }
+         console.log(count);
+    return count;
+  }
