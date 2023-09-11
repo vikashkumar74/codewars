b4fe0368e0c6389657a627a5a1dd52c4b7e07a3c
vikashkumar <vikashpoddar437@gmail.com>
2023-09-12 00:01:37 +0530

    ascii-table

--- 8kyu/first.js
+++ 8kyu/first.js
@@ -1,3 +1,20 @@
+
+// QUESTION //////////////////////////////
+
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17
+
+////////////////////////////////////////////////////////////////////////////
+// SOLUTION
 function countSheeps(sheep) {
     // TODO
      var count=0;
