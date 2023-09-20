072e8e45f28205e74ff739c832ab1bebef7d0488
vikashkumar <vikashpoddar437@gmail.com>
2023-09-21 00:24:12 +0530

    8kyu

---
+++ 8kyu/get ascii value of character.js
@@ -0,0 +1,12 @@
+// QUESTION //////////////////////////////
+
+// Get ASCII value of a character.
+
+// For the ASCII table you can refer to http://www.asciitable.com/
+
+////////////////////////////////////////////////////////////////////////////
+// SOLUTION
+
+function getASCII(c){
+    return c.charCodeAt(0);
+  }
