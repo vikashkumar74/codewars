b4b11b201c1171f968433a632c17eba2c5b0ffdb
vikashkumar <vikashpoddar437@gmail.com>
2023-10-05 21:43:43 +0530

    8kyu

---
+++ 8kyu/Hex to Decimal.js
@@ -0,0 +1,9 @@
+// QUESTION //////////////////////////////
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+////////////////////////////////////////////////////////////////////////////
+// SOLUTION
+function hexToDec(hexString){
+    //your code here
+    return parseInt(hexString,16)
+  }
