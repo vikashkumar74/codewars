db337e33cd5c2ed64116d71451875986d496f74f
vikashkumar <vikashpoddar437@gmail.com>
2023-10-16 23:52:49 +0530

    8kyu

---
+++ 8kyu/makeuppercase.js
@@ -0,0 +1,10 @@
+// QUESTION //////////////////////////////
+// Write a function which converts the input string to uppercase.
+
+
+////////////////////////////////////////////////////////////////////////////
+// SOLUTION
+function makeUpperCase(str) {
+    // Code here
+    return str.toUpperCase()
+  }
