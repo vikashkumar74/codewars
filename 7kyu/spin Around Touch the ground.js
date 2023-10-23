6091fb4db8a91478b6009f5ce56626e9bbeb0aa6
vikashkumar <vikashpoddar437@gmail.com>
2023-10-23 22:06:40 +0530

    7kyu

---
+++ 7kyu/spin Around Touch the ground.js
@@ -0,0 +1,30 @@
+// QUESTION //////////////////////////////
+// Given a list of directions to spin, "left" or "right", return an integer of how many full 360° rotations were made. Note that each word in the array counts as a 90° rotation in that direction.
+
+// Worked Example
+// ["right", "right", "right", "right", "left", "right"] ➞ 1
+// # You spun right 4 times (90 * 4 = 360)
+// # You spun left once (360 - 90 = 270)
+// # But you spun right once more to make a full rotation (270 + 90 = 360)
+// Examples
+// ["left", "right", "left", "right"] ➞ 0
+
+// ["right", "right", "right", "right", "right", "right", "right", "right"] ➞ 2
+
+// ["left", "left", "left", "left"] ➞ 1
+// Notes
+// Return a positive number.
+
+////////////////////////////////////////////////////////////////////////////
+// SOLUTION
+function spinAround(turns) {
+    // Here be dragons!
+    const l=turns.filter(e=>e=='left').length;
+    const r=turns.filter(e=>e=='right').length;
+    const spin=Math.abs(l-r)*90;
+    if(spin<360||turns==""){
+      return 0;
+    }else{
+    return Math.trunc(spin/360);
+    }
+  }
