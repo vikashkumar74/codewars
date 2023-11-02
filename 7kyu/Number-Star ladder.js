f7e680b4fa475ac866f5164fc88fe36c21d6f90c
vikashkumar <vikashpoddar437@gmail.com>
2023-11-02 18:18:15 +0530

    7kyu

--- 7kyu/Number-Star ladder.js
+++ 7kyu/Number-Star ladder.js
@@ -42,3 +42,11 @@ function pattern(n){
      }
     return output;
    }
+   ////////////////////////////////////////////// or /////////////////////////////////////////
+   var pattern = (n) =>{
+    var result = '1'
+    for (var i = 2; i <= n; i++){
+      result += '\n1' + '*'.repeat(i-1) + i
+    }
+    return result
+  }
