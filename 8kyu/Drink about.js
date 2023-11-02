94782a1fd8697c9b6adccfdc77ec597394d3db51
vikashkumar <vikashpoddar437@gmail.com>
2023-11-02 18:11:45 +0530

    7kyu

--- 8kyu/Drink about.js
+++ 8kyu/Drink about.js
@@ -1,23 +1,23 @@
 // QUESTION //////////////////////////////
-Kids drink toddy.
-Teens drink coke.
-Young adults drink beer.
-Adults drink whisky.
-Make a function that receive age, and return what they drink.
+// Kids drink toddy.
+// Teens drink coke.
+// Young adults drink beer.
+// Adults drink whisky.
+// Make a function that receive age, and return what they drink.
 
-Rules:
+// Rules:
 
-Children under 14 old.
-Teens under 18 old.
-Young under 21 old.
-Adults have 21 or more.
-Examples: (Input --> Output)
+// Children under 14 old.
+// Teens under 18 old.
+// Young under 21 old.
+// Adults have 21 or more.
+// Examples: (Input --> Output)
 
-13 --> "drink toddy"
-17 --> "drink coke"
-18 --> "drink beer"
-20 --> "drink beer"
-30 --> "drink whisky"
+// 13 --> "drink toddy"
+// 17 --> "drink coke"
+// 18 --> "drink beer"
+// 20 --> "drink beer"
+// 30 --> "drink whisky"
 
 ////////////////////////////////////////////////////////////////////////////
 // SOLUTION
