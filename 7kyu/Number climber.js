7eaf33eaa09b3fb236ee8d4b1a3b173a5f299e65
vikashkumar <vikashpoddar437@gmail.com>
2023-11-04 23:22:11 +0530

    7kyu

---
+++ 7kyu/Number climber.js
@@ -0,0 +1,25 @@
+// QUESTION //////////////////////////////
+// For every positive integer N, there exists a unique sequence starting with 1 and ending 
+// with N and such that every number in the sequence is either the double of the preceeding number or the double plus 1.
+
+// For example, given N = 13, the sequence is [1, 3, 6, 13], because . . . :
+
+//  3 =  2*1 +1
+//  6 =  2*3
+//  13 = 2*6 +1
+// Write a function that returns this sequence given a number N.
+//  Try generating the elements of the resulting list in ascending order, i.e.,
+//   without resorting to a list reversal or prependig the elements to a list.
+
+////////////////////////////////////////////////////////////////////////////
+// SOLUTION
+function climb(n){
+    // ...
+    let arr=[];
+      arr.push(Math.trunc(n))
+    while(n>1){
+      arr.push(Math.trunc(n/2));
+      n=Math.trunc(n/2);
+    }
+    return arr.sort((a,b)=>a-b)
+  }
