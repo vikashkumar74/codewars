7dde7c3adf09b095548fb4f9d476b52c3c40de99
vikashkumar <vikashpoddar437@gmail.com>
2023-11-11 23:46:04 +0530

    7kyu

--- 7kyu/reodering.js
+++ 7kyu/reodering.js
@@ -1,22 +1,22 @@
 // QUESTION //////////////////////////////
-There is a sentence which has a mistake in its ordering.
+// There is a sentence which has a mistake in its ordering.
 
-The part with a capital letter should be the first word.
+// The part with a capital letter should be the first word.
 
-Please build a function for re-ordering
+// Please build a function for re-ordering
 
-Examples
->>> re_ordering('ming Yao')
-'Yao ming'
+// Examples
+// >>> re_ordering('ming Yao')
+// 'Yao ming'
 
->>> re_ordering('Mano donowana')
-'Mano donowana'
+// >>> re_ordering('Mano donowana')
+// 'Mano donowana'
 
->>> re_ordering('wario LoBan hello')
-'LoBan wario hello'
+// >>> re_ordering('wario LoBan hello')
+// 'LoBan wario hello'
 
->>> re_ordering('bull color pig Patrick')
-'Patrick bull color pig'
+// >>> re_ordering('bull color pig Patrick')
+// 'Patrick bull color pig'
 ////////////////////////////////////////////////////////////////////////////
 // SOLUTION
 
