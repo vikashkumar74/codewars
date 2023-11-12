aec7ff63041f0533a7dd0f0915ea6e36070b180f
vikashkumar <vikashpoddar437@gmail.com>
2023-11-12 23:50:13 +0530

    7kyu

---
+++ 7kyu/array2binaryadd.js
@@ -0,0 +1,19 @@
+// QUESTION //////////////////////////////
+
+// Given an array containing only integers, add all the elements and return the binary equivalent of that sum.
+
+// If the array contains any non-integer element (e.g. an object, a float, a string and so on), return false.
+
+// Note: The sum of an empty array is zero.
+
+// arr2bin([1,2]) == '11'
+// arr2bin([1,2,'a']) == false
+
+////////////////////////////////////////////////////////////////////////////
+// SOLUTION
+function arr2bin(arr){
+    if (arr.some((x) => typeof x != "number"))
+    return false;
+  return arr.reduce((a, b) => a + b, 0).toString(2);
+//GET STARTED
+}
