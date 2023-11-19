9aa95c2d27555c298c4d89ddbae0b18e6bcba19d
vikashkumar <vikashpoddar437@gmail.com>
2023-11-19 22:57:33 +0530

    7kyu

--- 7kyu/sexy prime.js
+++ 7kyu/sexy prime.js
@@ -1,3 +1,4 @@
+// QUESTION //////////////////////////////
 // Please build a function for re-ordering
 // Sexy primes are pairs of two primes that are 6 apart. In this kata, your job is to complete the function which returns true if x & y are sexy, false otherwise.
 
